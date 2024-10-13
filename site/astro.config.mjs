5692a478e24e55367566c93c5e857b28f72bf16d
jacksondewalt <jdewalt80@gmail.com>
2024-10-13 12:41:23 -0400

    updated

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: '/wp_custom_update_manager/',  // Replace with your repo name
+    outDir: 'dist',
+});
